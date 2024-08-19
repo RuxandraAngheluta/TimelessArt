d2f599312980f1f98b8f0ac91dcb4248a9a58eec
angheluta ruxandra <anghelutaruxandra88@gmail.com>
2024-08-20 02:35:41 +0300

    add backend

---
+++ backend/timelessArt/src/main/java/repo/AccountRepo.java
@@ -0,0 +1,30 @@
+package repo;
+
+
+import entity.AccountEntity;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AccountRepo extends JpaRepository<AccountEntity,Long> {
+    Optional<AccountEntity> findByEmail(String email);
+
+    @Query("SELECT a FROM AccountEntity a WHERE a.token = :token")
+    AccountEntity findByToken(@Param("token") String token);
+    @Query("SELECT a FROM AccountEntity a WHERE a.validAccount = true")
+    List<AccountEntity> findByValidAccountTrue();
+    @Query("SELECT a FROM AccountEntity a WHERE a.validAccount = false")
+    List<AccountEntity> findByValidAccountFalse();
+
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE AccountEntity a SET a.validAccount = :isValid WHERE a.id = :id")
+    void updateAccountValidity(@Param("id") Long id, @Param("isValid") boolean isValid);
+
+}
